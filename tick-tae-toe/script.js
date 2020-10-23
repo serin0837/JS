8de533218bb6497d0f9dcc45d958a6374923ae4e
Serin <serin0837@yahoo.com>
2020-10-23 13:47:40 +0100

    finish tick tae toe

--- tick-tae-toe/script.js
+++ tick-tae-toe/script.js
@@ -1,8 +1,9 @@
 //place the marks
 const X_CLASS ="x"
 const CIRCLE_CLASS= "circle"
-
 const board = document.getElementById("board")
+
+//7. create winning combination array
 const WINNING_COMBINATION=[
   [0,1,2],
   [3,4,5],
@@ -13,19 +14,36 @@ const WINNING_COMBINATION=[
   [0,4,8],
   [2,4,6]
 ]
-
-const winningMessageTextElement = document.querySelector("[ data-winning-message-text]")
+//10. 
+const winningMessageTextElement = document.querySelector("[data-winning-message-text]")
 const winningMessageElement = document.getElementById("winningMessage")
 const cellElements = document.querySelectorAll("[data-cell]")
+//12. restart
+const restartButton=document.getElementById("restartButton")
 
 
 //turn true circle false x
 let circleTurn
 
-cellElements.forEach(cell=>{
+//6. call startGame
+startGame()
+
+//12. restart
+restartButton.addEventListener("click",startGame)
+//5. use startGame func
+function startGame(){
+  circleTurn=false
+  cellElements.forEach(cell=>{
+    //13. remove classes and click 
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener("click",handleClick)
     cell.addEventListener("click",handleClick,{once:true})//once:true is only fire event once!
 })
-
+setBoardHoverClass()
+//13. even click nothing happen because we did not remove show class
+winningMessageElement.classList.remove("show")
+}
 
 function handleClick(e){
   //1. place mark
@@ -34,39 +52,44 @@ function handleClick(e){
   //4. switch turns
   const cell = e.target//whatever we click
   const currentClass = circleTurn? CIRCLE_CLASS:X_CLASS
+  //1 mark
   placeMArk(cell,currentClass)
-  swapTurns()
-
-  //hover class
-  setBoardHoverClass()
-
+  
+  //8.checkWin
   if(checkWin(currentClass)){
-    endGame(false)
+    //9.endGame
+    endGame(false)//not draw so win
+    //11. isDraw
   }else if(isDraw()){
-    endGame(true)
+    endGame(true)//draw
   }else{
-    swapTurns()
-    setBoardHoverClass()
+    swapTurns()//2.swap turn
+    setBoardHoverClass()//3. set hover class
   } 
 }
-// can not understand.....
+// 9. endGame
 function endGame(draw){
   if(draw){
-    winningMessageElement.innerText="Draw!"
+    //11. -1
+    winningMessageTextElement.innerText="Draw!"
   }else{
-    winningMessageTextElement.innerText=`${circleTurn?"O's":"X's"} Wins!`
+    //10. winningMessageTextElement
+    winningMessageTextElement.innerText=`${circleTurn ? "O's":"X's"} Wins!`
+    console.log(circleTurn)
   }
   winningMessageElement.classList.add("show")
 }
+
+//11.-2
+function isDraw(){
+  return [...cellElements].every(cell=>{
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
+}
+
 //placeMark func//add x or circle
 function placeMArk(cell,currentClass){
   cell.classList.add(currentClass)
-}
-
-function isDraw(){
-  return [...cellElements].every(cell=>{
-    return cell.classList.contains(X_CLASS)||cell.classList.contains(CIRCLE_CLASS)
-  })
 }
 
 function swapTurns(){
@@ -81,8 +104,9 @@ function setBoardHoverClass(){
   }else{
     board.classList.add(X_CLASS)
   }
+  //4. when we start game we want to show hover too!
 }
-//not get it //
+//8. checkWin// don't understand
 function checkWin(currentClass){
   return WINNING_COMBINATION.some(combination=>{
     return combination.every(index=>{
