3408c2584b52e5b79f1818ceca6db5b57e47d7b2
Serin <serin0837@yahoo.com>
2020-12-09 12:43:46 +0000

    finish dom dice game

--- Project/dice game/script.js
+++ Project/dice game/script.js
@@ -4,11 +4,13 @@
 // How to read from the DOM
 //how to changes CSS styles
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0 // 0 first player 1 second player
+//replace with init function
+init();
+// scores = [0, 0];
+// roundScore = 0;
+// activePlayer = 0 // 0 first player 1 second player
 
 // dice = Math.floor(Math.random() * 6) + 1
 
@@ -28,55 +30,46 @@ activePlayer = 0 // 0 first player 1 second player
 
 // queryselector to change CSS
 //use style for css
-document.querySelector(".dice").style.display = "none"
+// document.querySelector(".dice").style.display = "none"
 
-//7. <event>
-// events: notifications that are sent to notify the code that something happende on weppage
-// examples; click button, resizing a window, scrolling down, pressing key...
-//event listner : a function that performs an action based on a certain event, It waits for a specific event to happen.
+// //7. <event>
+// // events: notifications that are sent to notify the code that something happende on weppage
+// // examples; click button, resizing a window, scrolling down, pressing key...
+// //event listner : a function that performs an action based on a certain event, It waits for a specific event to happen.
 
-//How events are processed?
-//execution context // execution stack is empty event working?
-//message queue
-//click event in message queue and function execution context 
-//7. how to set up an even handler
-//what callback function is
-//what an anonymous function is
-//another way to select elements by ID
-//How to change the image in an <img> element
-document.getElementById("score--0").textContent = 0
-//we don't use hash tag//different way to select element
-document.getElementById("score--1").textContent = 0
-document.getElementById("current--0").textContent = 0
-document.getElementById("current--1").textContent = 0
+// //How events are processed?
+// //execution context // execution stack is empty event working?
+// //message queue
+// //click event in message queue and function execution context 
+// //7. how to set up an even handler
+// //what callback function is
+// //what an anonymous function is
+// //another way to select elements by ID
+// //How to change the image in an <img> element
+// document.getElementById("score--0").textContent = 0
+// //we don't use hash tag//different way to select element
+// document.getElementById("score--1").textContent = 0
+// document.getElementById("current--0").textContent = 0
+// document.getElementById("current--1").textContent = 0
 
 document.querySelector(".btn--roll").addEventListener("click", function(){
-    // 1. Random number
-    let dice = Math.floor(Math.random() * 6) + 1
+    if(gamePlaying){
+        // 1. Random number
+        let dice = Math.floor(Math.random() * 6) + 1
 
-    //2. Display the result
-    let diceDOM = document.querySelector(".dice")
-    diceDOM.style.display = "block"
-    diceDOM.src = "./images/dice-"+ dice+".png"
-    //3. update the round score If the rolled number was Not a 1
-    if (dice !== 1) {
-        //Add score 
-        roundScore += dice;
-        document.querySelector("#current--"+ activePlayer).textContent = roundScore;
-    } else {
-        //Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-        roundScore = 0
-
-        document.getElementById("current--0").textContent = "0"
-        document.getElementById("current--1").textContent = "0"
-
-        document.querySelector(".player--0").classList.toggle("player--active")
-        document.querySelector(".player--1").classList.toggle("player--active")
-
-        // document.querySelector(".player--0").classList.remove("player--active")
-        // document.querySelector(".player--1").classList.add("player--active")
-        document.querySelector(".dice").style.display = "none"
+        //2. Display the result
+        let diceDOM = document.querySelector(".dice")
+        diceDOM.style.display = "block"
+        diceDOM.src = "./images/dice-"+ dice+".png"
+        //3. update the round score If the rolled number was Not a 1
+        if (dice !== 1) {
+            //Add score 
+            roundScore += dice;
+            document.querySelector("#current--"+ activePlayer).textContent = roundScore;
+        } else {
+            //Next player
+            nextPlayer()
+        }
     }
 })
 //we don't have to call a fucntion in args in addEventListner becuase
@@ -89,25 +82,80 @@ document.querySelector(".btn--roll").addEventListener("click", function(){
 // what the ternary operator is 
 //how to add, remove and toggle HTML classes
 
-//9. to be continue!
+//9. <DRY principle>
+//how to use function to correctly apply the DRY principle
+// HOw to think about the game logic like a programmer.
+
+document.querySelector(".btn--hold").addEventListener("click", function(){
+    if(gamePlaying){
+        //Add current score to global score
+        scores[activePlayer] += roundScore;
+        //Update the UI
+        document.querySelector("#score--" + activePlayer).textContent = scores[activePlayer]
+        //Check if player won the game
+        if(scores[activePlayer] >= 20){
+            document.querySelector("#name--" + activePlayer).textContent = "Winner!"
+            document.querySelector(".dice").style.display = "none"
+            document.querySelector(".player--" + activePlayer).classList.add("player--winner")
+            document.querySelector(".player--" + activePlayer).classList.remove("player--active")
+            gamePlaying = false
+        }else {
+                //nextPlayer
+            nextPlayer()
+        } 
+    }
+})
+
+//we make function becuase we don't want to repeat our self
+// just call the function that we made in each time 
+function  nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+    roundScore = 0
+
+    document.getElementById("current--0").textContent = "0"
+    document.getElementById("current--1").textContent = "0"
+
+    document.querySelector(".player--0").classList.toggle("player--active")
+    document.querySelector(".player--1").classList.toggle("player--active")
+
+    // document.querySelector(".player--0").classList.remove("player--active")
+    // document.querySelector(".player--1").classList.add("player--active")
+    // not remove and add just use toggle method
+    document.querySelector(".dice").style.display = "none"
+}
 
 
 
+//<10. Creating a game ...>
+document.querySelector(".btn--new").addEventListener("click",init)
+    //create init function 
+
+function init(){
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+    document.querySelector(".dice").style.display = "none"
+    document.getElementById("score--0").textContent = 0
+    document.getElementById("score--1").textContent = 0
+    document.getElementById("current--0").textContent = 0
+    document.getElementById("current--1").textContent = 0
+    //change back to normal player name
+    document.querySelector("#name--0").textContent = "Player 1"
+    document.querySelector("#name--1").textContent = "Player 2"
+    document.querySelector(".player--0").classList.remove("player--winner")
+    document.querySelector(".player--1").classList.remove("player--winner")
+    document.querySelector(".player--0").classList.remove("player--active")
+    document.querySelector(".player--1").classList.remove("player--active")
+    document.querySelector(".player--0").classList.add("player--active")
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+//<11.finishing the end>
+//state variables // programming in general
+// is our game playing or not? 
+// add palyingGame variable
 
 
 
